c56f50721e489469afea571435e9dcd2d45f5f6a
ctaciki <154457806+ctaciki@users.noreply.github.com>
2026-06-07 16:55:24 +0300

    Delete drone-builder (3) directory

--- drone-builder (3)/src/modes/mode.js
+++
@@ -1,8 +0,0 @@
-export class Mode {
-    constructor(name) {
-        this.name = name;
-        this.active = false;
-    }
-    enter(data) { this.active = true; }
-    exit() { this.active = false; }
-}
